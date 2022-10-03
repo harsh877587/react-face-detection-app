6ff2124fc10e6782bc9633d1a952d5564782acb6
Harsh <iamharshkumar1@gmail.com>
2022-10-03 18:36:07 +0530

    First commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer'>
-            <a  className='f4 link dim light-gray pointer' href='https://github.com/HarshKumar2001/react-face-detection-app' target="_blank" rel="noopener noreferrer">Git-hub</a>
+            <a  className='f4 link dim light-gray pointer' href='https://github.com/harsh877587/react-face-detection-app' target="_blank" rel="noopener noreferrer">Git-hub</a>
         </div>
     );
 }
